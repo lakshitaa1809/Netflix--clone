982c896140001253f7f94907f6819cb8d515bedb
SARANYA <tsaranya03@gmail.com>
2023-03-29 08:15:35 +0530

    add

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQQPag7nL9Np0OQmO5nlXL4TJSulZSHHk",
+  authDomain: "netflix-clone-f666b.firebaseapp.com",
+  projectId: "netflix-clone-f666b",
+  storageBucket: "netflix-clone-f666b.appspot.com",
+  messagingSenderId: "67522926753",
+  appId: "1:67522926753:web:0441f1c3f5dd2c466cf8e0",
+  measurementId: "G-0M1LT0E12C",
+};
